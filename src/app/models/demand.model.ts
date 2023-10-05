27822362a0a028ff9132f22d069035940e02400a
danielcamucatto <daniel.camucatto@gmail.com>
2023-10-05 17:20:05 -0300

    feat: added entity demand

---
+++ src/app/models/demand.model.ts
@@ -0,0 +1,16 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity()
+export class Demamd {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    decription: string;
+
+    @Column()
+    data: Date;
+
+
+}
