16a1f1750dcfc2aa18653acd5b60de09109ba043
danielcamucatto <daniel.camucatto@gmail.com>
2023-10-06 20:26:14 -0300

    added cors in projetc

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', 
+    credentials: true
+  })
 
   const config = new DocumentBuilder()
     .setTitle('Latinhas API')
