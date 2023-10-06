a7d51ca24d4ff4816b30c39c33a983039196c7cf
danielcamucatto <daniel.camucatto@gmail.com>
2023-10-06 14:47:46 -0300

    feat: added swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Latinhas API')
+    .setDescription('Documentação das rotas da API')
+    .setVersion('0.0.1')
+    .addTag('API')
+    .build(); 
+
+    const document = SwaggerModule.createDocument(app, config); 
+    SwaggerModule.setup('Swagger', app, document);
   await app.listen(3001);
 }
 bootstrap();
